18a747232554f7a0d72949e64e90cc1de59d0d14
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-12-20 17:56:57 -0600

    minor fixes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { notes, nextId } = require('../../db/db.json');
-const { addNewNote, validateNote, deleteNote } = require('../../lib/notes');
+const { addNewNote, validateNote } = require('../../lib/notes');
 
 // `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
 router.get('/notes', (req, res) => {
@@ -24,28 +24,5 @@ router.post('/notes', (req, res) => {
 		res.json(req.body);
 	}
 });
-
-// This route is for delete requests
-// router.delete('/notes/:id', (req, res) => {
-// 	// This takes the id from the query parameter
-// 	let deleteId = parseInt(req.params.id);
-// 	// This looks for the index of the note based on the queried id
-// 	let deleteIndex = notes.findIndex(x => {
-// 		return x.id === deleteId;
-// 	});
-// 	// Just in case, it doesn't allow Delete requests that can't match with an index
-// 	if (deleteIndex === -1) {
-// 		res.sendStatus(404);
-// 	}
-// 	// Sends index, notes array, and the nextId to deleteNote
-// 	else {
-// 		deleteNote(deleteIndex, notes, nextId);
-// 		res.status(200).json({
-// 			code: 200,
-// 			message: 'Note Deleted',
-// 			noteId: deleteId
-// 		});
-// 	}
-// });
 
 module.exports = router;
