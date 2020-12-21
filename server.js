1e3b40cabbe0c2b9ad304c1c740eb261c450d904
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-12-21 09:51:14 -0600

    syntax fix

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+
+// we create these consts as a route that the front-end can request data from
 const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
 
+// instantiate the server
+// we assign this to the app variable so that we can later chain on methods to the express.js server... (app.listen() for example)
 const app = express();
 const PORT = process.env.PORT || 3003;
 
@@ -20,6 +24,8 @@ app.use('/', htmlRoutes);
 // uses the router on the api distributor
 app.use('/api', apiRoutes);
 
+//this is the method we use to make our server listen... '.listen()'
+//what is a port? it  is like a specific room on a college campus... (campus = host)
 app.listen(PORT, () => {
 	console.log(`API server now on port http://localhost:${PORT}`);
 });
