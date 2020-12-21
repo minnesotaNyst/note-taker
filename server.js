dd4a86c1291661d2b831988ed3330038047ca54e
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-12-21 17:22:54 -0600

    adding more comments to the server.js file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+// *This file should be in charge of starting our server and should be located in the root of the repository
 const express = require('express');
 
 // we create these consts as a route that the front-end can request data from
+// The require() statements will read the index.js files in each of the directories indicated. This mechanism works the same way as directory navigation does in a website: If we navigate to a directory that doesn't have an index.html file, then the contents are displayed in a directory listing. But if there's an index.html file, then it is read and its HTML is displayed instead. Similarly, with require(), the index.js file will be the default file read if no other file is provided, which is the coding method we're using here.
 const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
 
@@ -19,9 +21,9 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+// This is our way of telling the server that any time a client navigates to <ourhost>/api, the app will use the router we set up in apiRoutes. If / is the endpoint, then the router will serve back our HTML routes.
 // uses the router on the html distributor
 app.use('/', htmlRoutes);
-
 // uses the router on the api distributor
 app.use('/api', apiRoutes);
 
