cdc268168f754585417a29fa3d213e8daa2b5829
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-12-25 21:07:24 -0600

    adding comments and refactoring

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -9,15 +9,13 @@ router.get('/notes', (req, res) => {
 
 // `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client
 router.post('/notes', (req, res) => {
-	// This takes a unique id from the db property "nextId"
+	// This takes a unique id from the property "nextId"
 	req.body.id = nextId;
 	// validates note format
 	if (!validateNote(req.body)) {
 		res
 			.status(400)
-			.send(
-				'Please make sure you enter a title and text for your note.'
-			);
+			.send('Please make sure you enter a title and text for your note.');
 	} else {
 		// if validation is successful, it passes req.body and notes array to addNewNote
 		const note = addNewNote(req.body, notes);
